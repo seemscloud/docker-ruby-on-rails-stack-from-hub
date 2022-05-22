545b62d2bc4a40f053a3a580e0cb97c6944ccb8e
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2022-05-22 22:50:11 +0200

    fdas

--- rails/docker-entrypoint.sh
+++ rails/docker-entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+rails new . \
+  --database postgresql \
+  --skip-system-test \
+  --skip-test
+
+bundler install
+
+rails db:migrate
+
 rails server \
   --port "${LISTEN_PORT}" \
   --environment "${ENVIRONMENT}" \
